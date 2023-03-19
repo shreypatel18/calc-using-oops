35de4633da122af2ef509c5a522b91dc01cd14d7
shreypatel18 <shap202318@gmail.com>
2023-03-20 00:00:47 +0530

    first version

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Optional;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+     shape rect1 = new rectangle();
+     Startwork rec = new Startwork(rect1);
+     shape cir1 = new circle();
+     Startwork circle = new Startwork(cir1);5
+    }
+}
+interface shape{
+    void input();
+    static void output(float result){
+        System.out.println("Area is equal to"+" "+result);
+    };
+    void calc();
+}
+
+class rectangle implements shape{
+    float lenght;
+    float breadth;
+    float result;
+    public void input(){
+
+        Scanner inp = new Scanner(System.in);
+        lenght = inp.nextFloat();
+        breadth = inp.nextFloat();
+    }
+
+    @Override
+    public void calc() {
+        result =lenght*breadth;
+        shape.output(result);
+    }
+
+}
+
+class circle implements shape{
+    float radius;
+    float result;
+
+    public void input(){
+        Scanner inp = new Scanner(System.in);
+        radius = inp.nextFloat();
+
+    }
+
+    @Override
+    public void calc() {
+        result =(22/7)*(radius*radius);
+        shape.output(result);
+
+    }
+
+}
+class Startwork{
+   Startwork(shape s1){
+   s1.input();
+   s1.calc();
+   }
+
+
+}
+
